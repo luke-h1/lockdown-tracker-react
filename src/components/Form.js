811e72be71a5ac9c9f48fd0244ca8ea335988b8d
luke-h1 <luke.howsam@yahoo.com>
2020-10-19 18:45:12 +0100

    add hooks to Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Form.css';
 
 const Form = ({ title }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (e) => setValue(e.target.value);
+
+  const handleClick = async (text) => {
+    const API_URL = `https://cors-anywhere.herokuapp.com/https://www.lockdownapi.com/${text}`;
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <Fragment>
       <div className="form-container">
-        <input type="text" placeholder="postcode" className="form__input" />
-        <input className="btn" type="submit" placeholder="submit" />
+        <input
+          type="text"
+          placeholder="postcode"
+          className="form__input"
+          onChange={onChange}
+        />
+        <input
+          className="btn"
+          type="submit"
+          placeholder="submit"
+          onClick={handleClick}
+        />
       </div>
     </Fragment>
   );
